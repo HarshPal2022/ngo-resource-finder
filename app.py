903bfafa83179b8eed854718af7de7cbbf920668
Harsh Ravikant Pal <palharrav21bcs@student.mes.ac.in>
2026-07-11 23:15:42 +0530

    Prepare project for Render deployment

--- app.py
+++ app.py
@@ -18,4 +18,4 @@ def health():
     })
 
 if __name__ == "__main__":
-    app.run(debug=True,use_reloader=False)
+    app.run(host="0.0.0.0", port=5000)
--- test_recommender.py
+++
@@ -1,6 +0,0 @@
-from utils.recommender import recommend
-
-print(recommend(
-    "Education Support for girls",
-    "Mumbai"
-))
