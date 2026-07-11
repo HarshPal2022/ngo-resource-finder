7ee6f63c238851d6afd6deafb36bf8a428e5df69
Harsh Ravikant Pal <palharrav21bcs@student.mes.ac.in>
2026-07-11 23:30:32 +0530

    Database correction

--- database/db_config.py
+++ database/db_config.py
@@ -3,9 +3,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
-MYSQL_PORT = int(os.getenv("MYSQL_PORT", 3306))
-MYSQL_USER = os.getenv("MYSQL_USER", "root")
-MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "")
-MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", "ngo_connect")
-MYSQL_SSL_CA = os.getenv("MYSQL_SSL_CA")
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_PORT = int(os.getenv("MYSQL_PORT"))
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
+
+MYSQL_SSL_CA = os.path.join(BASE_DIR, "ssl", "isrgrootx1.pem")
